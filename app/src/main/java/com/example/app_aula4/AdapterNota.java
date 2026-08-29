fe6519091343ce3fa0b2a19b9fe827c59eddc0a5
doomzyangek <enzobartolo9030@gmail.com>
2026-08-28 21:24:46 -0300

    snapshot 21:24 aula 4

--- app/src/main/java/com/example/app_aula4/AdapterNota.java
+++ app/src/main/java/com/example/app_aula4/AdapterNota.java
@@ -1,4 +1,52 @@
 package com.example.app_aula4;
 
-public class AdapterNota {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class AdapterNota extends RecyclerView.Adapter<AdapterNota.ViewHolder> {
+
+    private List<Nota> listaNotas;
+    public AdapterNota(List<Nota> dados){
+        this.listaNotas = dados;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //faz o vinculo do card que vamos usar
+        View card = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_nota,parent,false);
+        return new ViewHolder(card);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //preenche os campos de cada card
+        Nota item = listaNotas.get(position);
+        holder.txtTitulo.setText(item.getTitulo());
+        holder.txtDescricao.setText(item.getDescricao());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaNotas.size();
+    }
+
+    // Permite vincular os objetos XML no codigo java
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txtTitulo, txtDescricao;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtTitulo = itemView.findViewById(R.id.txtTitulo);
+            txtDescricao = itemView.findViewById(R.id.txtDescricao);
+        }
+    }
 }
